d6dc5e098055923cce728a6c05a3d7e09060da84
James Dabajo <90013822+MrPinkPanther10@users.noreply.github.com>
2022-02-10 09:59:05 -0500

    added images

---
+++ public/js/script.js
@@ -0,0 +1,50 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow, i) => {
+    const itemNumber = movieLists[i].querySelectorAll("img").length;
+    let clickCounter = 0;
+    arrow.addEventListener("click", () => {
+        const ratio = Math.floor(window.innerWidth / 270);
+        clickCounter++;
+        if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
+            movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+      }px)`;
+        } else {
+            movieLists[i].style.transform = "translateX(0)";
+            clickCounter = 0;
+        }
+    });
+
+    console.log(Math.floor(window.innerWidth / 270));
+});
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
