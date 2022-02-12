67044cc66ede52fa1fcb7ff805c8c99c34dc0e8c
cheesecake_assassin <santasebastian@yahoo.com>
2022-02-12 00:17:29 -0500

    added logout function

--- public/js/script.js
+++ public/js/script.js
@@ -40,11 +40,13 @@ function showSlides(n) {
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        [i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    if (slideIndex > 1 ) {
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].className += ' active';
+    }
 }
